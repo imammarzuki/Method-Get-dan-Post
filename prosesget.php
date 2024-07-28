2481eac1635859924ce7103e6868a950723ce7bd
Imam Marzuki <imammarzuki32@gmail.com>
2024-07-28 15:35:07 +0700

    Add files via upload

---
+++ prosesget.php
@@ -0,0 +1,6 @@
+<?php
+$nama=$_GET['nama'];
+echo "nama kamu adalah $nama";
+echo "<br><br><br>";
+echo "<a href='get.html'>Kembali</a>";
+?>
---
+++ prosespost.php
@@ -0,0 +1,6 @@
+<?php
+$nama=$_POST['nama'];
+echo "nama kamu adalah $nama";
+echo "<br><br><br>";
+echo "<a href='post.html'>Kembali</a>";
+?>
